c42703260b1b13124b0b256accef079aff891625
Gagan Tyagi <gagan2u2002@gmail.com>
2018-05-07 11:57:08 +0530

    comments updated

--- src/main/java/com/etechon/lambdastream/App.java
+++ src/main/java/com/etechon/lambdastream/App.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.function.Consumer;
 
 /**
- * This is basic code for students so that the will know the better way for 
+ * This is basic code for students so that they will know the better way for 
  * traverse the list.
  * Difference show here between , for , for-each , consumer and stream implementation.
  *
@@ -16,20 +16,20 @@ public class App
     {
     	List<Integer> myList= Arrays.asList(1,2,3,4,5,10,45);
     	
-    	// External Iterator
+    	// External Iterator - try to avaoid this JAVA 7
     	System.out.println("Value from for loop");
     	for (int i=0;i<myList.size();i++) {
     		
     		System.out.println(myList.get(i));
     	}
     	
-       // More significant way - but again it is a external Iterator
+       // More significant way - but again it is a external Iterator - JAVA 7
     	System.out.println("Value from for each loop");
     	for (Integer value : myList) {
 			System.out.println(value);
 		}
     	
-    	//internal iterator
+    	//internal iterator - supported in Java8
     	System.out.println("Value from for each consumer");
     	myList.forEach(new Consumer<Integer>() {
 			@Override
@@ -39,7 +39,7 @@ public class App
     		
 		});
     	
-    	//stream implementation
+    	//stream implementation - supported in JAVA 8
     	System.out.println("Value from Stream Implemenation");
     	myList.stream().forEach(i->System.out.println(i));
     	
